529f0119eb5204c20c357313a41fdcd419680e26
noctildon <noctildon2@gmail.com>
2024-10-10 09:52:13 -0500

    test github actions 5

--- cicd.py
+++ cicd.py
@@ -1,13 +1,7 @@
-import sys
-
 def write():
-    with open('file.txt', 'w') as f:
+    with open('output.txt', 'w') as f:
         f.write('Hello from GitHub Actions!!')
 
-def main():
-    print("Hello from GitHub Actions!")
-    print('Python version: ', sys.version)
-    write()
-
 if __name__ == "__main__":
-    main()
+    write()
+    print('Written to output.txt')
