1032cf58d2d0e7fa7f43ee9edff7ba1369f61729
noctildon <noctildon2@gmail.com>
2025-10-29 18:23:39 -0500

    remove python cicd file

--- cicd.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def write():
-    with open('output.txt', 'w') as f:
-        f.write(f'Hello from GitHub Actions!! @ {time.ctime()}')
-
-if __name__ == "__main__":
-    write()
-    print('Written to output.txt')
